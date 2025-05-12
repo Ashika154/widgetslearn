8bf487b54d32a1a51a36ab207425cf5666f9347b
Ashika154 <146411438+Ashika154@users.noreply.github.com>
2025-05-12 20:28:47 +0530

    Add files via upload

--- lib/drag.dart
+++ lib/drag.dart
@@ -8,37 +8,89 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  List<String> items = ['Cold Coffee', 'Hot Coffee', 'Espresso'];
+  List<String> items = ['Cold Coffee', 'Hot Coffee', 'Cold Water'];
   String? draggedItem;
+  bool isSwitched = true;
+  bool isSwitch = true;
+
+  bool _isItemVisible(String item) {
+    if (item.toLowerCase().contains("hot")) {
+      return isSwitched;
+    } else if (item.toLowerCase().contains("cold")) {
+      return isSwitch;
+    }
+    return true;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:  Color(0xFFA1887F),
+      backgroundColor: Color(0xFFA1887F),
       appBar: AppBar(
-        leading:  Icon(Icons.arrow_back_ios_new),
-        title:  Text("Cupfulcage"),
+        leading: Icon(Icons.arrow_back_ios_new),
+         title: Text("Cupfulcage"),
         centerTitle: true,
       ),
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-             Padding(
+            Padding(
               padding: EdgeInsets.all(15.0),
-              child: Text(
-                "My Cart",
+              child: Text("My Cart",
                 style: TextStyle(
-                   fontSize: 20,
+                  fontSize: 20,
                   fontWeight: FontWeight.bold,
                   color: Colors.brown,
                 ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: Row(
+                children: [
+                  Text("Hot Drinks",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.brown),
+                  ),
+                  Spacer(),
+                  Switch(
+                    value: isSwitched,
+                    onChanged: (value) {
+                      setState(() {
+                            isSwitched = value;
+                      });
+                    },
+                    activeColor: Colors.brown,
+                    inactiveThumbColor: Colors.brown,
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: Row(
+                children: [
+                  Text(
+
+                    "Cold Drinks",
+                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.brown),
+                  ),
+                  Spacer(),
+                  Switch(
+                    value: isSwitch,
+                    onChanged: (value) {
+                      setState(() {
+                        isSwitch = value;
+                      });
+                    },
+                    activeColor: Colors.brown,
+                    inactiveThumbColor: Colors.brown,
+                  )],
               ),),
-
-
+            SizedBox(height: 25),
             Expanded(
               child: ReorderableListView(
-                padding:  EdgeInsets.symmetric(horizontal: 8),
+                padding: EdgeInsets.symmetric(horizontal: 8),
                 onReorder: (oldIndex, newIndex) {
                   setState(() {
                     if (newIndex > oldIndex) {
@@ -46,68 +98,72 @@ class _MainPageState extends State<MainPage> {
                     }
                     final item = items.removeAt(oldIndex);
                     items.insert(newIndex, item);
-                   });},
-                children: [
-                  for (int index = 0; index < items.length; index++)
-                    Padding(
-                      key: ValueKey(items[index] + index.toString()),
-                      padding:  EdgeInsets.all(8.0),
-                      child: Draggable<String>(
-                        data: items[index],
-                         onDragStarted: () => draggedItem = items[index],
-                        onDraggableCanceled: (_, __) => draggedItem = null,
-                        onDragEnd: (_) => draggedItem = null,
-                        feedback: Material(
-                          color: Colors.transparent,
-                          child: SizedBox(
-                            width: MediaQuery.of(context).size.width * 0.9,
-                            child: _buildListTile(items[index]),
-                          ),
-                        ),
-                        childWhenDragging: Opacity(
-                          opacity: 0.3,
-                          child: _buildListTile(items[index]),
-                        ),
-                        child: _buildListTile(items[index]),
-                      ),
-                    ),
-                ],
-              ),),
-
-             Spacer(),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: DragTarget<String>(
-                onAccept: (data) {
-                  setState(() {items.remove(data);
-                    draggedItem = null;
                   });
                 },
+                children: [
+                  for (int index = 0; index < items.length; index++)
+                    Visibility(
+                      key: ValueKey(items[index] + index.toString()),
+                      visible: _isItemVisible(items[index]),
+                      child: Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Draggable<String>(
+                          data: items[index],
+                          onDragStarted: () => draggedItem = items[index],
+                          onDraggableCanceled: (_, __) => draggedItem = null,
+                          onDragEnd: (_) => draggedItem = null,
+                          feedback: Material(
+                            color: Colors.transparent,
+                            child: SizedBox(width: MediaQuery.of(context).size.width * 0.9,
+                              child: _buildListTile(items[index]),
+                            ),
+                          ),
+                          childWhenDragging: Opacity(
+                            opacity: 0.3,
+                            child: _buildListTile(items[index]),
+                          ),
+                          child: _buildListTile(items[index]),
+                        ),),
+                    ),],
+              ),
+
+
+            ),
+            Spacer(),
+            Align(alignment: Alignment.bottomRight,
+              child: DragTarget<String>(
+                onAccept: (data) {
+                  setState(() {
+                    items.remove(data);
+                    draggedItem = null;
+                  });},
                 builder: (context, candidateData, rejectedData) {
                   return Padding(
-                    padding:  EdgeInsets.all(16.0),
+                    padding: EdgeInsets.all(16.0),
                     child: FloatingActionButton(
                       onPressed: () {},
                       backgroundColor:
                       candidateData.isNotEmpty ? Colors.red : Colors.brown,
-                      child:  Icon(Icons.delete),
+                      child: Icon(Icons.delete),
                     ),
-                  );},
+                  );
+                },
               ),
             ),
           ],
-           ),
+        ),
       ),
     );
   }
+
   Widget _buildListTile(String title) {
     return ListTile(
-        tileColor:  Color(0xFFF5E6DA),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(19)),
-       leading: Container(
+      tileColor: Color(0xFFF5E6DA),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(19)),
+      leading: Container(
         height: 200,
         width: 50,
-        decoration:  BoxDecoration(
+        decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage("assets/coffee.jpg"),
             fit: BoxFit.cover,
@@ -116,13 +172,13 @@ class _MainPageState extends State<MainPage> {
       ),
       title: Text(
         title,
-        style:  TextStyle(
+        style: TextStyle(
           fontSize: 15,
           fontWeight: FontWeight.bold,
           color: Colors.brown,
         ),
       ),
-      subtitle:  Text(
+      subtitle: Text(
         "\$250",
         style: TextStyle(fontSize: 15, color: Colors.brown),
       ),
@@ -132,8 +188,10 @@ class _MainPageState extends State<MainPage> {
           height: 40,
           width: 40,
           color: Colors.black26,
-          child:  Icon(Icons.add),
+          child: Icon(Icons.add),
         ),
-      ),);
+      ),
+
+    );
   }
 }
